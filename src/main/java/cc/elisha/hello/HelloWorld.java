35478a1d0df8dc5a41a3765d5059d9fc4e979d68
IElisha <I@Elisha.cc>
2019-03-24 08:59:38 +0800

    我的第一个git程序

---
+++ src/main/java/cc/elisha/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package cc.elisha.hello;
+
+/**
+ * git的第一个程序
+ * @author Elisha
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
